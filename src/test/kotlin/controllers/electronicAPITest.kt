343bcedf9f6f1a2981bcb3f5eb8dd46d7d6e6bd5
Adam <adambrazzill01@gmail.com>
2023-12-03 18:54:13 +0000

    fixed junit tests

--- src/test/kotlin/controllers/electronicAPITest.kt
+++ src/test/kotlin/controllers/electronicAPITest.kt
@@ -1,6 +1,14 @@
 
+import controllers.ElectronicAPI
+import models.Electronics
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import persistence.XMLSerializer
+import java.io.File
 
-/*
+
 class ElectronicAPITest {
     private lateinit var electronic1: Electronics
     private lateinit var electronic2: Electronics
@@ -9,6 +17,7 @@ class ElectronicAPITest {
 
     @BeforeEach
     fun setUp() {
+        electronicAPI = ElectronicAPI(XMLSerializer(File("electronics.xml")))
         // Initialize the Electronics and ElectronicAPI before each test
         electronic1 = Electronics(
             electronicId = 0,
@@ -37,7 +46,6 @@ class ElectronicAPITest {
             reorderLevel = 6
         )
 
-
         electronicAPI.addElectronic(electronic1)
         electronicAPI.addElectronic(electronic2)
         electronicAPI.addElectronic(electronic3)
@@ -55,7 +63,7 @@ class ElectronicAPITest {
         )
 
         assertTrue(electronicAPI.addElectronic(electronic))
-        assertEquals(1, electronicAPI.numberOfElectronics())
+        assertEquals(4, electronicAPI.numberOfElectronics())
     }
 
     @Test
@@ -72,15 +80,21 @@ class ElectronicAPITest {
 
         electronicAPI.addElectronic(electronic)
         assertTrue(electronicAPI.isValidElectronicId(0))
-        assertFalse(electronicAPI.isValidElectronicId(1))
+        assertTrue(electronicAPI.isValidElectronicId(1))
     }
 
     @Test
     fun testListAllElectronics() {
-        assertEquals("No electronics stored", electronicAPI.listAllElectronics())
+        val expectedOutput = """
+        |0: Electronics(electronicId=0, productCode=ELECTRONIC1, type=Type1, unitCost=100.0, numberInStock=10, reorderLevel=5, transactions=[])
+        |1: Electronics(electronicId=1, productCode=ELECTRONIC2, type=Type2, unitCost=150.0, numberInStock=8, reorderLevel=3, transactions=[])
+        |2: Electronics(electronicId=2, productCode=ELECTRONIC3, type=Type1, unitCost=120.0, numberInStock=12, reorderLevel=6, transactions=[])
+    """.trimMargin()
+
+        assertEquals(expectedOutput, electronicAPI.listAllElectronics())
 
         val electronic = Electronics(
-            electronicId = 0,
+            electronicId = 3,
             productCode = "TEST123",
             type = "Test Type",
             unitCost = 50.0,
@@ -90,11 +104,17 @@ class ElectronicAPITest {
 
         electronicAPI.addElectronic(electronic)
 
-        val expectedOutput = "0: $electronic"
-        assertEquals(expectedOutput, electronicAPI.listAllElectronics())
+        val updatedExpectedOutput = """
+        |0: Electronics(electronicId=0, productCode=ELECTRONIC1, type=Type1, unitCost=100.0, numberInStock=10, reorderLevel=5, transactions=[])
+        |1: Electronics(electronicId=1, productCode=ELECTRONIC2, type=Type2, unitCost=150.0, numberInStock=8, reorderLevel=3, transactions=[])
+        |2: Electronics(electronicId=2, productCode=ELECTRONIC3, type=Type1, unitCost=120.0, numberInStock=12, reorderLevel=6, transactions=[])
+        |3: Electronics(electronicId=3, productCode=TEST123, type=Test Type, unitCost=50.0, numberInStock=10, reorderLevel=5, transactions=[])
+    """.trimMargin()
+
+        assertEquals(updatedExpectedOutput, electronicAPI.listAllElectronics())
     }
 
 
 }
-*/
+
 
